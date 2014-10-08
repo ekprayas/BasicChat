b37c6ad94e38cd142bffbb37fd2f8baf76f89721
ekprayas <amit@ekprayas.com>
2014-10-08 15:12:53 +0530

    Its Hold Beans Now

---
+++ src/org/chatapp/common/Score.java
@@ -0,0 +1,92 @@
+/**
+ * 
+ */
+package org.chatapp.common;
+
+/**
+ * @author amsachan
+ *
+ */
+public class Score {
+
+	
+	private long happyScore;
+	private long angerScore;
+	private long fearScore;
+	private long sadScore;
+	private long surpriseScore;
+	private long disgustScore;
+	/**
+	 * @return the happyScore
+	 */
+	public long getHappyScore() {
+		return happyScore;
+	}
+	/**
+	 * @param happyScore the happyScore to set
+	 */
+	public void setHappyScore(long happyScore) {
+		this.happyScore = happyScore;
+	}
+	/**
+	 * @return the angerScore
+	 */
+	public long getAngerScore() {
+		return angerScore;
+	}
+	/**
+	 * @param angerScore the angerScore to set
+	 */
+	public void setAngerScore(long angerScore) {
+		this.angerScore = angerScore;
+	}
+	/**
+	 * @return the fearScore
+	 */
+	public long getFearScore() {
+		return fearScore;
+	}
+	/**
+	 * @param fearScore the fearScore to set
+	 */
+	public void setFearScore(long fearScore) {
+		this.fearScore = fearScore;
+	}
+	/**
+	 * @return the sadScore
+	 */
+	public long getSadScore() {
+		return sadScore;
+	}
+	/**
+	 * @param sadScore the sadScore to set
+	 */
+	public void setSadScore(long sadScore) {
+		this.sadScore = sadScore;
+	}
+	/**
+	 * @return the surpriseScore
+	 */
+	public long getSurpriseScore() {
+		return surpriseScore;
+	}
+	/**
+	 * @param surpriseScore the surpriseScore to set
+	 */
+	public void setSurpriseScore(long surpriseScore) {
+		this.surpriseScore = surpriseScore;
+	}
+	/**
+	 * @return the disgustScore
+	 */
+	public long getDisgustScore() {
+		return disgustScore;
+	}
+	/**
+	 * @param disgustScore the disgustScore to set
+	 */
+	public void setDisgustScore(long disgustScore) {
+		this.disgustScore = disgustScore;
+	}
+	
+}
